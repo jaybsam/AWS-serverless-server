7231968501c4cc6db56011eb92e120d9100a46b0
jaybsam <joshua_sam_b@outlook.com>
2022-11-23 00:21:51 +0800

    first commit "Server"

---
+++ config/db.js
@@ -0,0 +1,6 @@
+// Set the connection string based from the config vars of the production server
+// To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
+
+module.exports = {
+  db: "mongodb+srv://treforce:VukV7GMkfPZPWfBL@cluster0.amtbksa.mongodb.net/?retryWrites=true&w=majority"
+};
